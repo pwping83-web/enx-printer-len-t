01c3ca0cf5164dae3bc6ba6f2f7ea394aeeb0254
pwping83 <pwping83@gmail.com>
2026-04-23 02:58:28 +0900

    chore: auto save 2026-04-22 17:58:28

--- ENX printer re/src/utils/cleanLogs.ts
+++
@@ -1,8 +0,0 @@
-// 콘솔 로그 정리 유틸리티
-export const cleanupLogs = () => {
-  // 프로덕션 환경에서는 console.log 비활성화
-  if (process.env.NODE_ENV === 'production') {
-    console.log = () => {};
-    console.warn = () => {};
-  }
-};
